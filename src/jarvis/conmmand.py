1c18a11e5d8071284437adf5f54665537ddbe487
sccuhha <aaaaaa.com>
2025-05-15 15:25:14 +0000

    Confirm line by line

--- src/jarvis/conmmand.py
+++ src/jarvis/conmmand.py
@@ -1,19 +1,25 @@
 from os import system
 from re import findall
-def conmmand(stream):
-    """
-    cut the string sent from the server.
-    most conmmand are preceded by $ or between ```  ```
-    """
-    if "$" in stream:
-        cutted_command = '\n'.join(findall(r"\$(.*?)\n", stream))
+
+def conmmand(llm_feedback):
+    print(llm_feedback)
+    cutted_command = '\n'
+    if '$' in llm_feedback:
+        cutted_command = findall(r"\$(.*?)\n", llm_feedback)
+    elif '`' in llm_feedback:
+        cutted_command = findall(r"```\n(.*?)\n```", llm_feedback)
     else:
-        cutted_command = '\n'.join(findall(r"\`(.*?)\`", stream))
+        cutted_command = findall(r"bash\n(.*?)\n```", llm_feedback)
     print(cutted_command)
-    y_or_not = input("[y/n/show]")
-    if y_or_not == 'y':
-        system(cutted_command)
-    elif y_or_not == 'show':
-        print(stream)
-    else:
-        print("type your conmmand")
+    for line in cutted_command:
+        print(f'\n{line}')
+        action = input('Implement the conmmand or show the entire text? [y/n/show]')
+        if action == 'y':
+            system(line)
+        elif action == 'show':
+            print(llm_feedback)
+            print(f"\n\n{line}")
+            implement_or_not = input('Implement the conmmand? [y/n]')
+            if implement_or_not == 'y':
+                system(line)    
+
