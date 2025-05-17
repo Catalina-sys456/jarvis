b64789bf51caede4234beba60a2e0237b84820d6
sccuhha <aaaaaa.com>
2025-05-17 12:34:47 +0000

    add config.yaml

--- src/jarvis/read_config.py
+++ src/jarvis/read_config.py
@@ -7,9 +7,12 @@ def open_file(file_name):
 
 def read_config():
     config_file = os.path.join(os.path.expanduser('~'), '.config/jarvis', 'config.yaml')
-    with open(config_file) as file:
-        configration = yaml.safe_load(file)
-        provider = configration['provider']
-        api_key = configration['api_key']
-        model = configration['model']
-    return provider, model, api_key
+    try:
+        with open(config_file) as file:
+            configration = yaml.safe_load(file)
+            provider = configration['provider']
+            api_key = configration['api_key']
+            model = configration['model']
+            return provider, model, api_key
+    except:
+        print('failed to read the config file, does ~/.config/jarvis/config.yaml exist?')
